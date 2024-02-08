378ba6bb63c48d09bd5dcf94d043fac7fb48e0f8
Techfall15 <102117551+Techfall15@users.noreply.github.com>
2024-02-07 22:45:18 -0500

    Edited the spawn system to spawn beams at a random Y position.

--- Assets/Scripts/Beam/BeamSpawnSystem.cs
+++ Assets/Scripts/Beam/BeamSpawnSystem.cs
@@ -20,17 +20,30 @@ public partial struct BeamSpawnSystem : ISystem
         {
             if(beamSpawnerData.ValueRO.timeUntilSpawn <= 0)
             {
-                // Spawn New Beam
+                // Get Random Y Position
+                float randomYPos    = UnityEngine.Random.Range(-4.5f, 4.5f);
+
+                // Get Reference To Spawn Position Data
+                float3 spawnDataPos = beamSpawnerData.ValueRO.spawnPosition;
+
+                // Set Random Y Position
+                beamSpawnerData.ValueRW.spawnPosition = new float3(spawnDataPos.x, randomYPos, 0f);
+
+                // Spawn In New Beam
                 Entity newBeam = ecb.Instantiate(beamSpawnerData.ValueRO.entityToSpawn);
-                // Get Spawn Position
-                
+
+                // Create New Spawn Position
                 LocalTransform newBeamTransform = new LocalTransform
                 {
                     Position = beamSpawnerData.ValueRO.spawnPosition,
                     Rotation = Quaternion.Euler(0f,0f,90f),
                     Scale = 1f
                 };
+
+                // Set New Spawn Position
                 ecb.SetComponent(newBeam, newBeamTransform);
+
+                // Reset Spawn Timer
                 beamSpawnerData.ValueRW.timeUntilSpawn = beamSpawnerData.ValueRW.timeBetweenSpawns;
             }
             else
