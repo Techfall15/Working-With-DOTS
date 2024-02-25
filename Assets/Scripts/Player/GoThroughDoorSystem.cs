bc4d5a552166aae9a2f18800e56e8703cef3ea37
Techfall15 <102117551+Techfall15@users.noreply.github.com>
2024-02-24 21:15:13 -0500

    Updated the GoThroughDoorSystem to use Ijobentity in order to take advantage of burst compile.

--- Assets/Scripts/Player/GoThroughDoorSystem.cs
+++ Assets/Scripts/Player/GoThroughDoorSystem.cs
@@ -4,6 +4,7 @@ using Unity.Transforms;
 using Unity.Mathematics;
 using Unity.Collections;
 using Unity.Burst;
+using Unity.Jobs;
 
 
 public partial struct GoThroughDoorSystem : ISystem
@@ -17,38 +18,44 @@ public partial struct GoThroughDoorSystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-        EntityCommandBuffer ecb                 = new EntityCommandBuffer(Allocator.TempJob);
-        SpawnQuestionMarkJob questionMarkJob    = new SpawnQuestionMarkJob() { ecb = ecb };
+        EntityCommandBuffer     ecb                 = new EntityCommandBuffer(Allocator.TempJob);
+        SpawnQuestionMarkJob    questionMarkJob     = new SpawnQuestionMarkJob() { ecb = ecb };
+        NativeArray<Entity>     targetDoor          = new NativeArray<Entity>(1, Allocator.TempJob);
+        SetTargetDoorJob        setTargetDoorJob    = new SetTargetDoorJob(){ targetDoor = targetDoor };
 
 
         // For each source of input information
         foreach(var inputData in SystemAPI.Query<RefRO<InputData>>())
         {
-            // Check if the player presses the 'goThroughDoor' button
-            if (inputData.ValueRO.goThoughDoor == false) continue;
-
-            foreach(var woodenDoorData in SystemAPI.Query<RefRW<WoodenDoorData>>())
-            {
-                if (woodenDoorData.ValueRO.isOpen == false) continue;
-                SetNewPlayerPositionJob newPlayerPosJob = new SetNewPlayerPositionJob()
-                {
-                    ecb = ecb,
-                    newPlayerPos = woodenDoorData.ValueRO.playerSpawnLocation,
-                };
-                UpdateFadeBoxJob updateFadeBoxJob = new UpdateFadeBoxJob() { newSpawnLocation = woodenDoorData.ValueRO.cameraSpawnLocation };
-
-                newPlayerPosJob.Schedule(state.Dependency).Complete();
-                updateFadeBoxJob.Schedule(state.Dependency).Complete();
-
-            }
-            questionMarkJob.Schedule(state.Dependency).Complete();
             
+            if (inputData.ValueRO.goThoughDoor == false) continue;                   // Check if the player presses the 'goThroughDoor' button
+
+            setTargetDoorJob.Schedule(state.Dependency).Complete();                   // Set targetDoor to the door that the player has opened
+            ScheduleMovementJobs(ref state, ecb, targetDoor);
+
+
+            questionMarkJob.Schedule(state.Dependency).Complete();                      // If able, spawn a question mark over the NPC's head when the player leaves their room       
         }
-
-
         ecb.Playback(state.EntityManager);
+    }
 
 
+    public void ScheduleMovementJobs(ref SystemState state, EntityCommandBuffer ecb, NativeArray<Entity> targetDoor)
+    {
+        if (state.EntityManager.HasComponent<WoodenDoorData>(targetDoor[0]))
+        {
+            WoodenDoorData doorData = state.EntityManager.GetComponentData<WoodenDoorData>(targetDoor[0]);
+            UpdateFadeBoxJob updateFadeBoxJob = new UpdateFadeBoxJob() { newSpawnLocation = doorData.cameraSpawnLocation };
+            SetNewPlayerPositionJob newPlayerPosJob = new SetNewPlayerPositionJob()
+            {
+                ecb = ecb,
+                newPlayerPos = doorData.playerSpawnLocation,
+            };
+
+
+            newPlayerPosJob.Schedule(state.Dependency).Complete();                 // Move the player
+            updateFadeBoxJob.Schedule(state.Dependency).Complete();                 // Move the fade box
+        }
     }
 
 }
@@ -63,12 +70,12 @@ public partial struct SpawnQuestionMarkJob : IJobEntity
 
         if(npcData.canSpawnQuestionMark == true && npcData.alreadySpawnedQuestionMark == false)
         {
-            Entity questionMark = ecb.Instantiate(npcData.questionMarkEntity);
-            LocalTransform questionMarkTransform = new LocalTransform()
+            Entity          questionMark            = ecb.Instantiate(npcData.questionMarkEntity);
+            LocalTransform  questionMarkTransform   = new LocalTransform()
             {
-                Position = new float3(npcTransform.Position.x, npcTransform.Position.y + 1.25f, npcTransform.Position.z),
-                Rotation = Quaternion.identity,
-                Scale = 1f
+                Position    = new float3(npcTransform.Position.x, npcTransform.Position.y + 1.25f, npcTransform.Position.z),
+                Rotation    = Quaternion.identity,
+                Scale       = 1f
             };
             npcData.alreadySpawnedQuestionMark = true;
             ecb.SetComponent<LocalTransform>(questionMark, questionMarkTransform);
@@ -103,7 +110,11 @@ public partial struct UpdateFadeBoxJob : IJobEntity
         fadeBoxData.newPosition = newSpawnLocation;
     }
 }
-
-
+[BurstCompile]
+public partial struct SetTargetDoorJob : IJobEntity
+{
+    public NativeArray<Entity> targetDoor;
+    public void Execute(ref WoodenDoorData doorData, Entity entity) => targetDoor[0] = (doorData.isOpen == true) ? entity : targetDoor[0];
+}
 
 #endregion
