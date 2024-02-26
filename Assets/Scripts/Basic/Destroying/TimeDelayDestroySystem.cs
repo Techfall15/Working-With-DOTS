508c0ab87accdd3ff01c4aa66b7a8f352c8da6af
Techfall15 <102117551+Techfall15@users.noreply.github.com>
2024-02-26 15:15:36 -0500

    Updated the time delay destroy system to use jobs with burst compile.

--- Assets/Scripts/Basic/Destroying/TimeDelayDestroySystem.cs
+++ Assets/Scripts/Basic/Destroying/TimeDelayDestroySystem.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using Unity.Entities;
 using Unity.Collections;
+using Unity.Burst;
+using static UnityEngine.EventSystems.EventTrigger;
+
 public partial struct TimeDelayDestroySystem : ISystem
 {
     public void OnCreate(ref SystemState state)
@@ -11,22 +14,28 @@ public partial struct TimeDelayDestroySystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-        EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.Temp);
-
-        foreach(var(timeDelayData, entity) in SystemAPI.Query<RefRW<TimeDelayDestroyData>>().WithEntityAccess())
+        EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.TempJob);
+        CountDownToDestroyJob countDown = new CountDownToDestroyJob()
         {
-            if(timeDelayData.ValueRO.timeUntilDestruction > 0)
-            {
-                timeDelayData.ValueRW.timeUntilDestruction -= SystemAPI.Time.DeltaTime;
-            }
-            else
-            {
-                ecb.DestroyEntity(entity);
-            }
-        }
+            ecb = ecb,
+            deltaTime = SystemAPI.Time.DeltaTime
+        };
 
-
+        countDown.Schedule(state.Dependency).Complete();
         ecb.Playback(state.EntityManager);
     }
 
+}
+[BurstCompile]
+public partial struct CountDownToDestroyJob : IJobEntity
+{
+    public EntityCommandBuffer ecb;
+    public float deltaTime;
+    public void Execute(ref TimeDelayDestroyData timeDestroyData, Entity entity)
+    {
+        if (timeDestroyData.timeUntilDestruction > 0) 
+            timeDestroyData.timeUntilDestruction -= deltaTime;
+        else 
+            ecb.DestroyEntity(entity);
+    }
 }
